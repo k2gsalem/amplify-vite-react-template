2b1bc73eaf880fd8958c02416b6b8ebd7c2ce660
gopinathnpos <gopinath@northernpos.com>
2025-04-09 23:08:13 +0530

    test

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
 
   return (
     <main>
-      <h1>Testy Gopi</h1>
+      <h1>Testy 2343</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (<li 
